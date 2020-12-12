a4e33e93f21d649df90fbfa6b77f5ae1a28c2355
octopuscart <dev@develop.com>
2020-12-13 01:02:32 +0530

    chagns

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -64,14 +64,14 @@
 
 
 
-<!--        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-2246837719135984",
                     enable_page_level_ads: true
             });
            
-        </script>-->
+        </script>
 
     </head>
 
